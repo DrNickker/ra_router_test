c7eab3402fa35248c132d88ad5221fe8b0891345
DrNickker <mintagirov2015@yandex.ru>
2023-07-27 16:05:16 +0500

    app_edit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import CreatePost from "./components/CreatePost";
+import ListPosts from "./components/ListPosts";
+import EditPost from "./components/EditPost";
+import { Link, Route, Routes } from "react-router-dom";
+import ButtonCreate from "./components/ButtonCreate";
+import Post from "./components/Post";
+import FooterPostButtons from './components/FooterPostButtons';
 
-function App() {
+export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [content, setContent] = useState('');
+  const [post, setPost] = useState(null);
+  const [postId, setId] = useState(0);
+  const [del, setDel] = useState(null);
+
+  function fetchGet() {
+    fetch(process.env.REACT_APP_SERVER)
+    .then(resp => resp.json())
+    .then(json => {
+      setPosts(json)
+    });
+  };
+
+  useEffect(() => {
+    fetchGet();
+  }, []);
+
+  useEffect(() => {
+    if (post !== null) {
+      function fetchPost() {
+        fetch(process.env.REACT_APP_SERVER, {
+          method: 'POST',
+          body: JSON.stringify(post)
+        })
+          .then(() => fetchGet());
+        setPost(null);
+      }
+      fetchPost();
+    };
+  }, [post]);
+
+  useEffect(() => {
+    function fetchDelete() {
+      fetch(process.env.REACT_APP_SERVER + del, {
+        method: 'DELETE'
+      })
+      .then(() => fetchGet());
+    };
+    fetchDelete();
+  }, [del]);
+
+  function publishPost() {
+    if (content !== '') {
+      setPost({
+        content: content,
+        id: 0
+      });
+      setContent('');
+    };
+  };
+
+  function removePost(id) {
+    setDel(id);
+  };
+
+  function inputCreate(ev) {
+    setContent(ev.target.value);
+  };
+
+  function closeCreate() {
+    setContent('');
+  };
+
+  function editPost(post) {
+    setPost({
+      content: post,
+      id: Number(postId)
+    });
+    setId(0);
+  };
+
+  function idPost(id) {
+    setId(id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-page">
+      <ButtonCreate/>
+      <Routes>
+        <Route path={'/'} element={<ListPosts posts={posts}/>}/>
+        <Route path={'/posts/new'} element={<CreatePost
+            publishPost={publishPost}
+            create={content}
+            handleInputCreate={inputCreate}
+            closeCreatePost={closeCreate}/>}/>
+        <Route path={'/posts/:postId'} element={
+            <Post close={<Link to={'/'} className="post-close"/>} posts={posts}>
+              <FooterPostButtons idPost={idPost} remove={removePost} />
+            </Post>
+          } />
+        <Route path={'/edit'} element={<EditPost posts={posts} id={postId} editPost={editPost}/>}/>
+      </Routes>
     </div>
   );
 }
 
-export default App;
